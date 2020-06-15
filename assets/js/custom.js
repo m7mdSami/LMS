f4c43c5581c6dd300ab13d7e1c10af0b7a4e7978
m7mdsami <samy.ashoor@gmail.com>
2020-06-16 01:19:30 +0200

    add color picker to change navbar color

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,6 +10,7 @@ $(document).ready(() => {
 });
 
 const handleThemeUpdate = (cssVars) => {
+    console.log(cssVars)
     const root = document.querySelector(':root');
     const keys = Object.keys(cssVars);
     keys.forEach(key => {
@@ -20,9 +21,10 @@ const handleThemeUpdate = (cssVars) => {
 // Check if there is a color saved in localstorage in browser
 let checkColorInStorage = () => {
     let color = localStorage.getItem(`color`);
-    if(color === null) {
+    if(color === null || color == 'undefined') {
         return;
     }
+    console.log(color)
     color = JSON.parse(color);
     handleThemeUpdate(color);
     return color;
@@ -30,7 +32,8 @@ let checkColorInStorage = () => {
 
 let changeColor = () => {
 
-    const checkColor = checkColorInStorage();
+    let checkColor = checkColorInStorage();
+    console.log(checkColor)
     const htmlCode = `<div class="card mb-1">
                         <div role="button" class="card-header" aria-controls="settings-mainNavbar" aria-expanded="true">
                             <h5 class="card-title">Change Color</h5>
@@ -49,7 +52,27 @@ let changeColor = () => {
                                 </fieldset>
                             </div>
                         </div>
-                    </div>`;
+                    </div>
+                    <div class="card mb-1">
+                        <div role="button" class="card-header" aria-controls="settings-mainNavbar" aria-expanded="true">
+                            <h5 class="card-title">MAIN NAVBAR</h5>
+                        </div>
+                        <div id="settings-mainNavbar" role="tabpanel" style="" class="collapse show">
+                            <div class="card-body">
+                                <fieldset id="__BVID__33" role="group" aria-labelledby="__BVID__33__BV_label_"
+                                    class="b-form-group form-group mb-0">
+                                    <div role="group" aria-labelledby="__BVID__33__BV_label_" class="">
+                                        <div id="group-mainNavbar-0" role="radiogroup" tabindex="-1" class="">
+                                            <div class="custom-control custom-radio custom-control-inline">
+                                                <input value="${checkColor ? checkColor['--dark'] : '#1d2126'}" class="input-color-dark cursor-pointer h-10 outline-none rounded-full w-10 input-color-picker" type="color" data-id="dark" name="Color">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div>
+                    </div>
+                    `;
 
     $('#app-settings .p-2').append(htmlCode);
 
@@ -64,11 +87,15 @@ let changeColor = () => {
             };
 
             if (checkColor) {
-                colorObj[cssPropName] = val;
+                checkColor[cssPropName] = val;
+            } else {
+                checkColor = colorObj
             }
+
+            console.log(checkColor)
             
-            let storge = localStorage.setItem(`color`, JSON.stringify(colorObj));
-            handleThemeUpdate(colorObj);
+            let storge = localStorage.setItem(`color`, JSON.stringify(checkColor));
+            handleThemeUpdate(checkColor);
         });
     });
 }
